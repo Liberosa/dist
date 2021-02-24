fbd03f98e4442bc324e8f7a9122340b01e02c1d3
Liberosa <40002607+Liberosa@users.noreply.github.com>
2021-02-24 05:30:29 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+//     $('.grid').slick({
+//         dots: true,
+//         infinite: true,
+//         speed: 500,
+//         fade: true,
+//         cssEase: 'linear',
+//         // swipe:true,
+//         swipeToSlide:true,
+//         arrows:true,
+//       });
+//   });
+
+  
